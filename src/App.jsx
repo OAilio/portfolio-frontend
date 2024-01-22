71a999079aefc1ca6fc81ceb4b3d77505d2e4fc3
Okko Ailio <oiaili@utu.fi>
2024-01-22 12:11:06 +0200

    Fonts & font sizes, landing zone done. Also unnecessary files deleted

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,68 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './css/basic.css'
+import './css/navbar.css'
+import './css/landing.css'
+import './css/fonts.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAnglesDown } from '@fortawesome/free-solid-svg-icons';
 
-function App() {
-  const [count, setCount] = useState(0)
+
+const Navbar = () => {
+  document.addEventListener("DOMContentLoaded", function () {
+    const burgerMenu = document.getElementById("burger-menu");
+    const navbarRight = document.getElementById("navbar-right");
+  
+    burgerMenu.addEventListener("click", function () {
+      navbarRight.style.display = navbarRight.style.display === "none" ? "flex" : "none";
+    })
+  })
 
   return (
+    <div className="navbar">
+      <div className="navbar-left">
+        <a href="#home">JOHN NAME</a>
+      </div>
+      <div className="navbar-right">
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+        <a href="#projects">Projects</a>
+        <a href="#contact-me">Contact me</a>
+      </div>
+      <div className="burger-menu" id="burger-menu">
+          <span className="bar"></span>
+          <span className="bar"></span>
+          <span className="bar"></span>
+      </div>
+    </div>
+  )
+}
+
+const LandingContent = () => {
+  return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="landing-content">
+        <div className="landing-text">
+          <h1>HELLO!</h1>
+          <h2>I am John Name</h2>
+          <p>A 3rd year CS student at the University of Turku, with an interest for UI/UX-design and web development</p>
+        </div>
+        <div className="my-picture">
+          <img src='./pictures/okko.jpg' alt='My Picture' />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="landing-bottom">
+        <h3>Get to know me!</h3>
+        <FontAwesomeIcon icon={faAnglesDown} fade className="down-arrows"/>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
+  )
+}
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <LandingContent />
+    </div>
   )
 }
 
