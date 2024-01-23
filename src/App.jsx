fb85e27c252282cf7e15c3154535bdfefb8f1bb1
Okko Ailio <oiaili@utu.fi>
2024-01-23 17:44:50 +0200

    Buttons added, about-segment complete for now

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+/* eslint-disable react/no-unescaped-entities */
 import './css/basic.css'
 import './css/navbar.css'
 import './css/landing.css'
 import './css/fonts.css'
+import './css/about.css'
+import './css/buttons.css'
+import './css/hireme.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAnglesDown } from '@fortawesome/free-solid-svg-icons';
 
@@ -43,17 +47,68 @@ const LandingContent = () => {
         <div className="landing-text">
           <h1>HELLO!</h1>
           <h2>I am John Name</h2>
-          <p>A 3rd year CS student at the University of Turku, with an interest for UI/UX-design and web development</p>
+          <hp>A 3rd year CS student at the University of Turku,
+            with an interest for UI/UX-design and web development</hp>
         </div>
         <div className="my-picture">
           <img src='./pictures/okko.jpg' alt='My Picture' />
         </div>
       </div>
       <div className="landing-bottom">
-        <h3>Get to know me!</h3>
-        <FontAwesomeIcon icon={faAnglesDown} fade className="down-arrows"/>
+        <h4>Get to know me!</h4>
+        <FontAwesomeIcon icon={faAnglesDown} className="down-arrows"/>
       </div>
     </>
+  )
+}
+
+const AboutMe = () => {
+  const listOfSkills = ["HTML", "CSS", "JavaScript", "Python", "Figma", "Java", "Canva",
+  "Scrum", "Office Programs", "WordPress", "Git", "React"]
+  
+  return (
+    <>
+      <div className="about-me-container">
+        <div className="about-me-header">
+          <h3>About Me</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Nunc sodales non dui sed posuere.</p>
+        </div>
+        <div className="about-me-content">
+          <div className="get-to-know">
+            <h3>Get to know me!</h3>
+            <p>I'm a 23-year-old computer science student at the University of Turku, originally from Helsinki.</p>
+            <p>As I'  m getting my bachelor's degree this spring, I am interested in diverse job opportunities, my largest interest being the already mentioned web development and UI/UX-design,
+              which I'm currently writing my bachelors thesis on.</p>
+            <p>If You're still wondering, whether to hire me, check out the next segment.
+              You can also contact me immediately below!</p>
+            <button className="blue-button">
+              Contact
+            </button>
+          </div>
+          <div className="my-skills">
+            <h3>My Skills</h3>
+            <div className="skills-list">
+              {listOfSkills.map(skill => (
+                <span key={skill} className="skill-tag">{skill}</span>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+const HireMe = () => {
+  return (
+    <div className="hire-me-container">
+      <div className="hire-me-header">
+      <h2>Why should You hire me?</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Nunc sodales non dui sed posuere.</p>
+      </div>
+    </div>
   )
 }
 
@@ -62,6 +117,8 @@ const App = () => {
     <div>
       <Navbar />
       <LandingContent />
+      <AboutMe />
+      <HireMe />
     </div>
   )
 }
