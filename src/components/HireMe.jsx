6a75f48aa827d40cb01d14d14f2a16a405a318c6
Okko Ailio <oiaili@utu.fi>
2024-02-02 13:55:01 +0200

    CV added

--- src/components/HireMe.jsx
+++ src/components/HireMe.jsx
@@ -33,7 +33,7 @@ function HireMe() {
         </div>
       </div>
       <div className="button-container">
-      <a className='blue-button' href='pictures/okko.jpg' target='_blank'>
+      <a className='blue-button' href='/CV OKKO AILIO ENG 2024 port.pdf' target='_blank'>
           Check out my CV
         </a>
       </div>
