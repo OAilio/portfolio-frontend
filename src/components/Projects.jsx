166af40e0d7aaebaba53ed2e28c95dc8d802cde8
Okko Ailio <oiaili@utu.fi>
2024-11-24 11:48:11 +0200

    Added design projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -13,21 +13,48 @@ function Projects() {
             name: `Mitäsöis`,
             image: `./mitasois.png`,
             description: `A MERN-stack app that helps the user store their cooking history and decide what to make when it's time to go grocery shopping.`,
-            link: `https://github.com/OAilio/mitasois` 
+            link: `https://github.com/OAilio/mitasois`,
+            button_text: "GitHub" 
+        },
+        {
+            id: 5,
+            name: `Acme Coffee`,
+            image: `./acmecoffee.png`,
+            description: `Design of a modern café website for tablet and mobile layouts, ensuring a seamless and user-friendly experience.`,
+            link: `https://www.figma.com/design/5je8RlUEOoxjrqaUNEDBbB/Okko-Ailio-Design-portfolio?node-id=1-2&t=3pT3HfyIjMEGj44V-1`,
+            button_text: "Figma" 
         },
         {
             id: 2,
             name: `Portfolio`,
             image: `./portfolio.png`,
             description: `My personal portfolio website, which I've created from scratch to display my web development skills and design potential.`,
-            link: `https://github.com/OAilio/portfolio-frontend` 
+            link: `https://github.com/OAilio/portfolio-frontend`,
+            button_text: "GitHub" 
+        },
+        {
+            id: 6,
+            name: `Menu design`,
+            image: `./ottergame.png`,
+            description: `An intuitive and visually appealing menu for a 2D platformer game as part of a university group project.`,
+            link: `https://www.figma.com/design/5je8RlUEOoxjrqaUNEDBbB/Okko-Ailio-Design-portfolio?node-id=1-5890&t=3pT3HfyIjMEGj44V-1`,
+            button_text: "Figma" 
         },
         {
             id: 3,
             name: `Contact Book`,
             image: `./contactbook.png`,
             description: `My first full-stack web project, a SPA contact management application created with React and Node.js.`,
-            link: `https://github.com/OAilio/contactBook` 
+            link: `https://github.com/OAilio/contactBook`,
+            button_text: "GitHub"
+        },
+        {
+            id: 7,
+            name: `Date Picker`,
+            image: `./input.png`,
+            description: `My design sketch of a robust and efficient input method for selecting a range of two dates, intended to minimize user errors.`,
+            link: `https://www.figma.com/design/5je8RlUEOoxjrqaUNEDBbB/Okko-Ailio-Design-portfolio?node-id=1-4666&t=3pT3HfyIjMEGj44V-1`,
+            button_text: "Figma"
         },
         {
             id: 4,
@@ -35,7 +62,16 @@ function Projects() {
             image: `./algoritmit.png`,
             description: `A first-year group project, which compares the sorting speeds of some of the most popular
             sorting algorithms.`,
-            link: `https://github.com/OAilio/spAlgorithmComparison` 
+            link: `https://github.com/OAilio/spAlgorithmComparison`,
+            button_text: "GitHub" 
+        },
+        {
+            id: 8,
+            name: `Shopping list`,
+            image: `./shoppinglist.png`,
+            description: `The initial sketch and detailed design prototype of a shopping list app, created to simplify the shopping process.`,
+            link: `https://www.figma.com/design/5je8RlUEOoxjrqaUNEDBbB/Okko-Ailio-Design-portfolio?node-id=1-3&t=3pT3HfyIjMEGj44V-1`,
+            button_text: "Figma" 
         }
     ]
     const settings = {
@@ -70,7 +106,7 @@ function Projects() {
                                         <p>{project.description}</p>
                                         <div className="button-container">
                                             <a href={project.link} target="_blank" rel="noopener noreferrer" className="blue-button">
-                                                GitHub
+                                                {project.button_text}
                                             </a>
                                         </div>
                                     </div>
